c4c01b3d5f1b4ce39614bf89126a6563d505e28f
QuantumKya <rohan.vats4nier@gmail.com>
2026-03-21 16:55:47 -0500

    fixed the SHAKY again!

--- shaky.glsl
+++ shaky.glsl
@@ -13,12 +13,14 @@ float random (vec2 co) {
 }
 
 void main() {
-    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    gl_Position = gl_Position + vec4(
-        (random(aVertexPosition.xy + uRandomAddend + vec2(0,0)) - 0.5) * 0.12,
-        (random(aVertexPosition.yz + uRandomAddend + vec2(0,1)) - 0.5) * 0.12,
-        (random(aVertexPosition.xz + uRandomAddend + vec2(0,2)) - 0.5) * 0.12,
+    const float a = 0.01; // 0.01 for cool style, 0.025 for kinda shaky, 0.05 for SHAKY
+
+    gl_Position = aVertexPosition + vec4(
+        (random(aVertexPosition.xy + uRandomAddend + vec2(0,0)) - 0.5) * a,
+        (random(aVertexPosition.yz + uRandomAddend + vec2(0,1)) - 0.5) * a,
+        (random(aVertexPosition.xz + uRandomAddend + vec2(0,2)) - 0.5) * a,
         0
     );
+    gl_Position = uProjectionMatrix * uModelViewMatrix * gl_Position;
     vColor = aVertexColor;
 }
